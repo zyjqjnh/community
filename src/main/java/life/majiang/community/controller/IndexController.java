05482d147f143df239847dac1514389d18a0f5c8
zhuyaojie <zyjqjnh@126.com>
2019-09-27 09:32:00 +0800

    add sessionInterceptoer

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -30,16 +30,6 @@ public class IndexController {
                       @RequestParam(name = "page", defaultValue = "1") Integer page,
                       @RequestParam(name = "size", defaultValue = "5") Integer size,
                       Model model) {
-    Cookie[] cookies = request.getCookies();
-    for (Cookie cookie : cookies) {
-      if (cookie.getName().equals("token")) {
-        User user = userMapper.getUserByToken(cookie.getValue());
-        if (user != null) {
-          request.getSession().setAttribute("user", user);
-        }
-        break;
-      }
-    }
     PaginationDTO pagination = questionService.list(page, size);
     model.addAttribute("pagination", pagination);
     return "index";
